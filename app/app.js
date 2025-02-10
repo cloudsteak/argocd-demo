bb95c0f284c6172f1804e09cf1b156067767da83
Tibor Kiss <tibor.kiss@vodafone.com>
2025-02-10 17:42:50 +0100

    App changes

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js 22 deployed with ArgoCD!');
+    res.send('Hello from Node.js 22 - deployed with ArgoCD!');
 });
 
 app.listen(PORT, () => {
